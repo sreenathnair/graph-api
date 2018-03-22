428cec7b092959e99d7ba8c503b8d92e0f697522
Sreenath Sasidharan Nair <sreenath@ebi.ac.uk>
2018-03-22 12:40:38 +0000

    commit

--- app/model.py
+++ app/model.py
@@ -2,9 +2,12 @@ from py2neo import Graph, Node, Relationship
 import os
 
 #url = os.environ.get('GRAPHENEDB_URL', 'http://miranda.ebi.ac.uk:7474')
-url = os.environ.get('GRAPHENEDB_URL', 'http://localhost:7474')
+#url = os.environ.get('GRAPHENEDB_URL', 'http://localhost:7474')
+#dest = 'bolt://miranda.ebi.ac.uk:7687'
+#dest = 'bolt://localhost:7687'
+dest = os.environ.get('DEST_URL')
 username = os.environ.get('NEO4J_USERNAME')
 password = os.environ.get('NEO4J_PASSWORD')
 
-graph = Graph(url + '/db/data/', username=username, password=password)
+graph = Graph(dest, username=username, password=password)
 
